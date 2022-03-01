0b356434f1a626f6712cc4dcf77f266826ea6503
AubieVikash <vikash.t@auberginesolutions.com>
2022-03-02 00:06:32 +0530

    read functionalities performed

--- src/index.js
+++ src/index.js
@@ -14,9 +14,33 @@ app.post('/user', (req, res)=>{
     const user = new User(req.body)
     // console.log(user)
     user.save().then(()=>{
-        res.status(200).send(user)
+        res.send(user)
     }).catch((error)=>{
         res.status(400).send(error.message)
+    })
+})
+
+//get all users
+app.get('/users', (req, res)=>{
+    User.find().then((users)=>{
+        res.send(users)
+    }).catch((error)=>{
+        res.status(400).send(error)
+    })
+})
+
+//get user by _id
+app.get('/users/:id', (req, res)=>{
+    const _id = req.params.id
+
+    User.findById(_id).then((user)=>{
+        if(!user){
+            return res.status(404).send('no such user found')
+        }
+
+        res.send(user)
+    }).catch((error)=>{
+        res.status(500).send('something went wrong')
     })
 })
 
@@ -26,9 +50,33 @@ app.post('/task', (req, res)=>{
     const task = new Task(req.body)
 
     task.save().then(()=>{
-        res.status(200).send(task)
+        res.send(task)
     }).catch((error)=>{
         res.status(400).send(error.message)
+    })
+})
+
+//get all tasks route
+app.get('/tasks', (req, res)=>{
+    Task.find().then((tasks)=>{
+        res.send(tasks)
+    }).catch((error)=>{
+        res.status(404).send(error)
+    })
+})
+
+//get task by _id route
+app.get('/tasks/:id', (req, res)=>{
+    const _id = req.params.id
+
+    Task.findById(_id).then((task)=>{
+        if(!task){
+            return res.status(404).send('no such task found')
+        }
+
+        res.send(task)
+    }).catch((error)=>{
+        res.status(500).send('something went wrong')
     })
 })
 
