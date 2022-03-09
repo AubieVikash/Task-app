23ebcb7e9f17e543140951b5aaf696858ab7880c
AubieVikash <vikash.t@auberginesolutions.com>
2022-03-09 11:11:45 +0530

    file upload imlemented

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,8 +1,10 @@
 const express =     require('express')
-const { append } = require('express/lib/response')
-const router =      new express.Router()
+const { append } =  require('express/lib/response')
 const User =        require('../models/user')
 const auth =        require('../middleware/auth')
+const multer =      require('multer')
+const sharp =       require('sharp')
+const router =      new express.Router()
 
 
 
@@ -108,6 +110,58 @@ router.delete('/users/profile', auth, async(req, res)=>{
         res.status(201).send('profile succesfully deleted')
     } catch (error) {
         res.status(500).send(error)
+    }
+})
+
+
+
+const upload = multer({
+    limits: {
+        fileSize: 1000000,
+        },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+
+//add user profile picture
+router.post('/users/profile/avatar', auth, upload.single('avatar'), async (req, res)=>{
+    const buffer = await sharp(req.file.buffer).resize({ width: 250, height: 250 }).png().toBuffer()
+
+    req.user.avatar = buffer
+    await req.user.save()
+    res.send('Image uploaded successfully')
+}, (error, req, res, next)=>{
+    res.status(400).send({error: error.message})
+})
+
+
+//delete user profile picture
+router.delete('/users/profile/avatar', auth, async (req, res)=>{
+    req.user.avatar = undefined
+    await req.user.save()
+    res.send()
+})
+
+//get user avatar
+router.get('/users/:id/avatar', async (req, res)=>{
+    try {
+        const user = await User.findById(req.params.id)
+
+        if(!user || !user.avatar) {
+            throw new Error()
+        }
+
+        res.set('Content-type', 'image/png')
+        res.send(user.avatar)
+
+    } catch (error) {
+        res.status(404).send()   
     }
 })
 
